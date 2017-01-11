476fe99890399cfccf12b62acdbefa49fe4b3451
arnaudm63 <arnaud.mestries@gmail.com>
2017-01-11 13:14:14 +0100

    Button management added - to be tested

--- SevSeg/test_inc_dec.ino
+++ SevSeg/test_inc_dec.ino
@@ -13,40 +13,46 @@
 //Create an instance of the object.
 SevSeg myDisplay;
 
+//Create constants
+  //This pinout is for a bubble dispaly
+       //Declare what pins are connected to the GND pins (cathodes)
+       const int digit1 = 8; //Pin 1
+       const int digit2 = 5; //Pin 10
+       const int digit3 = 11; //Pin 4
+       const int digit4 = 13; //Pin 6
+       
+       //Declare what pins are connected to the segments (anodes)
+       const int segA = 7; //Pin 12
+       const int segB = 6; //Pin 11
+       const int segC = 10; //Pin 3
+       const int segD = 3; //Pin 8
+       const int segE = 9; //Pin 2
+       const int segF = 4; //Pin 9
+       const int segG = 2; //Pin 7
+       const int segDP= 12; //Pin 5
+   
+  const int numberOfDigits = 2; //Do you have a 1, 2 or 4 digit display?
+  // Constants for buttons
+  const int button_plus_pin = 13;
+  const int button_minus_pin = 14;
+
+
+
 //Create global variables
 unsigned long timer;
+int button_plus_State = 0;         // variable for reading the pushbutton status
+int button_minus_State = 0;         // variable for reading the pushbutton status
 
 // Setup
 void setup()
 {
-
   int displayType = COMMON_CATHODE; //Your display is either common cathode or common anode
-
-  
-  //This pinout is for a bubble dispaly
-       //Declare what pins are connected to the GND pins (cathodes)
-       int digit1 = 8; //Pin 1
-       int digit2 = 5; //Pin 10
-       int digit3 = 11; //Pin 4
-       int digit4 = 13; //Pin 6
-       
-       //Declare what pins are connected to the segments (anodes)
-       int segA = 7; //Pin 12
-       int segB = 6; //Pin 11
-       int segC = 10; //Pin 3
-       int segD = 3; //Pin 8
-       int segE = 9; //Pin 2
-       int segF = 4; //Pin 9
-       int segG = 2; //Pin 7
-       int segDP= 12; //Pin 5
-   
-  int numberOfDigits = 2; //Do you have a 1, 2 or 4 digit display?
-
   myDisplay.Begin(displayType, numberOfDigits, digit1, digit2, digit3, digit4, segA, segB, segC, segD, segE, segF, segG, segDP);
-  
   myDisplay.SetBrightness(100); //Set the display to 100% brightness level
-
   timer = millis();
+    // initialize the pushbutton pin as an input:
+  pinMode(button_plus_Pin, INPUT);
+  pinMode(button_minus_Pin, INPUT);
 }
 
 // Loop
